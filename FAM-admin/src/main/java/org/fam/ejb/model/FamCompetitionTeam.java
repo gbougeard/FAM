a7f54de123ed60d0728deb0098201578a7584dfc
Greg Bougeard <gbougeard@gmail.com>
2012-07-26 02:00:33 +0200

    Composition

--- FAM-admin/src/main/java/org/fam/ejb/model/FamCompetitionTeam.java
+++ FAM-admin/src/main/java/org/fam/ejb/model/FamCompetitionTeam.java
@@ -7,7 +7,16 @@ package org.fam.ejb.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
@@ -18,8 +27,11 @@ import java.io.Serializable;
 @Entity
 @Table(name = FamCompetitionTeam.TABLE_NAME)
 @NamedQueries({
-        @NamedQuery(name = "FamCompetitionTeam.findAll", query = "SELECT f FROM FamCompetitionTeam f"),
-        @NamedQuery(name = "FamCompetitionTeam.findByIdCompetitionTeam", query = "SELECT f FROM FamCompetitionTeam f WHERE f.idCompetitionTeam = :idCompetitionTeam")})
+               @NamedQuery(name = "FamCompetitionTeam.findAll",
+                           query = "SELECT f FROM FamCompetitionTeam f"),
+               @NamedQuery(name = "FamCompetitionTeam.findByIdCompetitionTeam",
+                           query = "SELECT f FROM FamCompetitionTeam f WHERE f.idCompetitionTeam = :idCompetitionTeam")
+})
 public class FamCompetitionTeam implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -32,9 +44,6 @@ public class FamCompetitionTeam implements Serializable {
      *
      */
     public static final String PROP_ID = "idCompetitionTeam";
-    /**
-     *
-     */
     public static final String COL_ID = "id_competition_team";
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,9 +54,6 @@ public class FamCompetitionTeam implements Serializable {
      *
      */
     public static final String COL_ID_SEASON = "id_season_competition";
-    /**
-     *
-     */
     public static final String PROP_SEASON = "famSeasonCompetition";
     @JoinColumn(name = COL_ID_SEASON, referencedColumnName = FamSeasonCompetition.COL_ID)
     @ManyToOne(optional = false)
@@ -58,9 +64,6 @@ public class FamCompetitionTeam implements Serializable {
      *
      */
     public static final String COL_ID_TEAM = "id_team";
-    /**
-     *
-     */
     public static final String PROP_TEAM = "famTeam";
     @JoinColumn(name = COL_ID_TEAM, referencedColumnName = FamTeam.COL_ID)
     @ManyToOne(optional = false)
